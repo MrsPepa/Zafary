2dc424fd3503c2e833d8b65a124ea9002299c972
MrsPepa <jose.leon1996@gmail.com>
2020-01-19 20:25:02 -0300

    Agregando Layout y estilos

---
+++ Layout/index.js
@@ -0,0 +1,22 @@
+const home = document.querySelector('#home');
+const wzp = document.querySelector('#whatsapp');
+const zpotify = document.querySelector('#zpotify');
+const titulo = document.querySelector('#tDinamico');
+const dinamico = document.querySelector('#dinamico');
+
+home.addEventListener('click', cargarHome);
+wzp.addEventListener('click', cargarWzp);
+zpotify.addEventListener('click', cargarZpotify);
+
+function cargarHome(){
+    dinamico.setAttribute('src', '../Home/index.html');
+    titulo.innerHTML = 'Zafary';
+}
+function cargarWzp(){
+    dinamico.setAttribute('src', './../Whatsapp/index.html');
+    titulo.innerHTML = 'Whatsapp';
+}
+function cargarZpotify(){
+    dinamico.setAttribute('src', './../Zpotify/index.html');
+    titulo.innerHTML = 'Zpotify';
+}
--- Login/index.js
+++ Login/index.js
@@ -1,22 +1,26 @@
 const email = window.document.querySelector('#correo');
 const password = window.document.querySelector('#pass');
+const errorMsj = window.document.querySelector('.msjError');
 const boton = window.document.querySelector('#btn');
 
 boton.addEventListener('click', validar);
+email.addEventListener('focus', focusInput);
+password.addEventListener('focus', focusInput);
 
 function validar(){
-	
-	const emailValue = email.value;
-	const passValue = password.value;
 
-	 if (emailValue == "ejemplo@gmail.com" && passValue == '123456') {
+	 if (email.value == "ejemplo@gmail.com" && password.value == '123456') {
         console.log('Iniciando sesión…');
-        email.value = '';
-        password.value = '';
-        email.focus();
-        password.focus();
+        
+        document.location.assign('./../Layout/index.html');
 
     } else {
+        //errorMsj.classList.remove(['hide']);
         console.log('Email y/o Contraseña incorrectos');
     }
+}
+
+function focusInput(){
+    //errorMsj.classList.remove(['visible']);
+    errorMsj.classList.add(['hide']);
 }
