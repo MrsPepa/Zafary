41fdc796f2cd5defccd491ef14b4ad2ae0220ed3
MrsPepa <jose.leon1996@gmail.com>
2020-01-19 22:42:22 -0300

    Arreglando login

--- Login/index.js
+++ Login/index.js
@@ -11,16 +11,14 @@ function validar(){
 
 	 if (email.value == "ejemplo@gmail.com" && password.value == '123456') {
         console.log('Iniciando sesión…');
-        
         document.location.assign('./../Layout/index.html');
 
     } else {
-        //errorMsj.classList.remove(['hide']);
+        errorMsj.classList.remove(['hide']);
         console.log('Email y/o Contraseña incorrectos');
     }
 }
 
 function focusInput(){
-    //errorMsj.classList.remove(['visible']);
     errorMsj.classList.add(['hide']);
 }
